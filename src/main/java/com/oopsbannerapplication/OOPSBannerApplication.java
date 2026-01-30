4507ef42c7c2ba73e5b5c86bfecd9d2b252e04e1
BL-Deepika <deepika.ramireddy@bridgelabz.com>
2026-01-30 11:42:08 +0530

    [Deepika] Added: UC3: Render OOPS as Banner using String.join()

---
+++ src/main/java/com/oopsbannerapplication/OOPSBannerApplication.java
@@ -0,0 +1,20 @@
+package com.oopsbannerapplication;
+/**
+ * OOPSBannerApp UC3 - OOPS Banner Display Application
+ * This class demonstrates a simple java application that displays the object-  * oriented programming system oops acronym to the console.
+ *@author Developer
+ * @version 3.0
+ */
+public class OOPSBannerApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ","   ***   " ,"   ***   ","******   ","   ***** "));
+        System.out.println(String.join(" "," **   ** " ," **   ** ","**    ** "," **      "));
+        System.out.println(String.join(" ","**     **" ,"**     **","**     **","**       "));
+        System.out.println(String.join(" ","**     **" ,"**     **","*******  ","  *****  "));
+        System.out.println(String.join(" ","**     **" ,"**     **","**       ","        ** "));
+        System.out.println(String.join(" "," **   ** " ," **   ** ","**       ","       ** "));
+        System.out.println(String.join(" ","   ***   " ,"   ***   ","**       ","  *****  "));
+    }
+}

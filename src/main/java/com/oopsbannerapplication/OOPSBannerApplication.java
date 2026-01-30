814bc5a3d43463cc20fb2fa8f4600dd4f61b9c72
BL-Deepika <deepika.ramireddy@bridgelabz.com>
2026-01-30 12:06:13 +0530

    [Deepika] Added: UC6: Refactor Banner Logic into Functions.

---
+++ src/main/java/com/oopsbannerapplication/OOPSBannerApplication.java
@@ -0,0 +1,57 @@
+package com.oopsbannerapplication;
+/**
+ * OOPSBannerApp UC6 - OOPS Banner Display Application
+ * This class demonstrates a simple java application that displays the object-  * oriented programming system oops acronym to the console.
+ *@author Developer
+ * @version 6.0
+ */
+public class OOPSBannerApplication {
+
+    public static String[] getOPattern(){
+        return new String[]{
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
+        };
+    }
+
+    public static String[] getPPattern(){
+        return new String[]{
+                "******   ",
+                "**    ** ",
+                "**     **",
+                "*******  ",
+                "**       ",
+                "**       ",
+                "**       "
+        };
+    }
+
+    public static String[] getSPattern(){
+        return new String[]{
+                "   ***** ",
+                " **      ",
+                "**       ",
+                "  *****  ",
+                "        ** ",
+                "       ** ",
+                "  *****  "
+
+        };
+    }
+
+    public static void main(String args[]){
+
+        String[] oPattern= getOPattern();
+        String[] pPattern= getPPattern();
+        String[] sPattern= getSPattern();
+
+        for(int i=0;i< oPattern.length;i++){
+            System.out.println(oPattern[i] + " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
+        }
+    }
+}

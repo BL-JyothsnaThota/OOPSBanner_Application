777edc2ac67c5653a16d1a49ab517f33340678b5
BL-Deepika <deepika.ramireddy@bridgelabz.com>
2026-01-30 11:49:23 +0530

    [Deepika] Added: UC4: Render OOPS as Banner using String Array and Loop.

---
+++ src/main/java/com/oopsbannerapplication/OOPSBannerApplication.java
@@ -0,0 +1,25 @@
+package com.oopsbannerapplication;
+/**
+ * OOPSBannerApp UC4 - OOPS Banner Display Application
+ * This class demonstrates a simple java application that displays the object-  * oriented programming system oops acronym to the console.
+ *@author Developer
+ * @version 4.0
+ */
+public class OOPSBannerApplication {
+
+    public static void main(String[] args) {
+
+        String lines[]=new String[7];
+        lines[0] = String.join(" ","   ***   " ,"   ***   ","******   ","   ***** ");
+        lines[1] = String.join(" "," **   ** " ," **   ** ","**    ** "," **      ");
+        lines[2] = String.join(" ","**     **" ,"**     **","**     **","**       ");
+        lines[3] = String.join(" ","**     **" ,"**     **","*******  ","  *****  ");
+        lines[4] = String.join(" ","**     **" ,"**     **","**       ","        ** ");
+        lines[5] = String.join(" "," **   ** " ," **   ** ","**       ","       ** ");
+        lines[6] = String.join(" ","   ***   " ,"   ***   ","**       ","  *****  ");
+
+        for(String line:lines){
+            System.out.println(line);
+        }
+    }
+}

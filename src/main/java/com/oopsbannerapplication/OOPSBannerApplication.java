376f48e2fafb2dbb1109672b3dfc60d4d82bc3b9
BL-Deepika <deepika.ramireddy@bridgelabz.com>
2026-01-30 11:31:28 +0530

    [Deepika] Added: UC2: Render OOPS as Banner using Print Statements.

---
+++ src/main/java/com/oopsbannerapplication/OOPSBannerApplication.java
@@ -0,0 +1,20 @@
+package com.oopsbannerapplication;
+
+/**
+ * OOPSBannerApp UC2 - OOPS Banner Display Application
+ * This class demonstrates a simple java application that displays the object-  * oriented programming system oops acronym to the console.
+ *@author Developer
+ * @version 2.0
+ */
+public class OOPSBannerApplication {
+
+    public static void main(String[] args) {
+        System.out.println("   ***   " + " " +"   ***   "+ " " +"******   "+ " "+"   ***** ");
+        System.out.println(" **   ** " + " " +" **   ** "+ " " +"**     ** "+ " "+"**      ");
+        System.out.println("**     **" + " " +"**     **"+ " " +"**     **"+ " "+" **       ");
+        System.out.println("**     **" + " " +"**     **"+ " " +"*******  "+ " "+"   *****  ");
+        System.out.println("**     **" + " " +"**     **"+ " " +"**       "+ " "+"        ** ");
+        System.out.println(" **   ** " + " " +" **   ** "+ " " +"**       "+ " "+"        ** ");
+        System.out.println("   ***   " + " " +"   ***   "+ " " +"**       "+ " "+"   *****  ");
+    }
+}

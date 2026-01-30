3e908269424cdf1265f48e367f11cb6e77cb66e2
BL-Deepika <deepika.ramireddy@bridgelabz.com>
2026-01-30 11:57:24 +0530

    [Deepika] Added: UC5:  Render OOPS as Banner using Inline Array Initialization.

---
+++ src/main/java/com.oopsbannerapplication/OOPSBannerApplication.java
@@ -0,0 +1,26 @@
+package com.oopsbannerapplication;
+/**
+ * OOPSBannerApp UC5 - OOPS Banner Display Application
+ * This class demonstrates a simple java application that displays the object-  * oriented programming system oops acronym to the console.
+ *@author Developer
+ * @version 5.0
+ */
+public class OOPSBannerApplication {
+
+    public static void main(String[] args) {
+
+        String lines[]= {
+                String.join(" ", "   ***   ", "   ***   ", "******   ", "   ***** "),
+                String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      "),
+                String.join(" ", "**     **", "**     **", "**     **", "**       "),
+                String.join(" ", "**     **", "**     **", "*******  ", "  *****  "),
+                String.join(" ", "**     **", "**     **", "**       ", "        **"),
+                String.join(" ", " **   ** ", " **   ** ", "**       ", "       ** "),
+                String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ")
+        };
+
+        for(String line:lines){
+            System.out.println(line);
+        }
+    }
+}
